ab4c1577ede5c5188f7593197aa3a3776a6d4ab5
Zach Banducci <zbducci@gmail.com>
2018-10-15 19:35:35 -0500

    Parent component imported to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule
