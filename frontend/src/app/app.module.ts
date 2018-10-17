59adc869d38c65c7305c3a4f0181796dc3f8191b
Zach Banducci <zbducci@gmail.com>
2018-10-17 10:54:43 -0500

    Implemented Routing architecture in app component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ParentComponent } from './parent/parent.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ParentComponent
+    ParentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
