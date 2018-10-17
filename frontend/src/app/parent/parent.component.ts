6370877eb2d0520ed2ea2f1c27e0b98834b6fdd9
Zach Banducci <zbducci@gmail.com>
2018-10-17 12:21:19 -0500

    Added basic table functionality and formatting

--- frontend/src/app/parent/parent.component.ts
+++ frontend/src/app/parent/parent.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  tasks: any;
+  members: any;
   constructor() { }
 
   ngOnInit() {
+    this.tasks = [
+      {id: 1, name: 'Mow the lawn', member: 'Jimbo', status: 'In Progress'},
+      {id: 2, name: 'Take out the trash', member: 'Janette', status: 'Completed'},
+      {id: 3, name: 'Run errands', member: 'Jimbo' , status: 'Pending verification' },
+      {id: 4, name: 'Walk the dogs', member: 'Jimbo', status: 'Completed'}
+    ];
+
+    this.members = [
+      {id: 1, name: 'John', type: 'Parent'},
+      {id: 2, name: 'Jane', type: 'Parent'},
+      {id: 3, name: 'Jimbo', type: 'Child'},
+      {id: 4, name: 'Janette', type: 'Child'}
+    ];
   }
 
 }
