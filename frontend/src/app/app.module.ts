6c1f27c53ac989aefb8920045dccf063471c0a1e
samgiles <samdotgiles@gmail.com>
2018-10-18 20:31:33 -0500

    added files to angular project, still need to incorporate routing, update forms to angular forms, and change the font for the headings

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NewaccountComponent } from './newaccount/newaccount.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewaccountComponent
   ],
   imports: [
     BrowserModule
