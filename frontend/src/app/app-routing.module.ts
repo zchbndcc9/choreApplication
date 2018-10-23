6fbf10e3223e4c45a91df9a2d87afb7148ebbc8a
samgiles <samdotgiles@gmail.com>
2018-10-23 10:28:43 -0500

    did some minor styling tweaks to match parent page and added some routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { NewaccountComponent } from './newaccount/newaccount.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 
 const routes: Routes = [
-  { path: 'family', component: ParentComponent }
+  { path: 'family', component: ParentComponent },
+  { path: 'new-account', component: NewaccountComponent}
 ];
 
 @NgModule({
