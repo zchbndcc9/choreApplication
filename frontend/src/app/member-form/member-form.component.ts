c3148a5a88337b12276fae5fed72e7fa9f3ba266
Zach Banducci <zbducci@gmail.com>
2018-10-22 19:12:14 -0500

    Added Member Form component

---
+++ frontend/src/app/member-form/member-form.component.ts
@@ -0,0 +1,30 @@
+import { PasswordValidator } from './../validators/password.validator';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-form',
+  templateUrl: './member-form.component.html',
+  styleUrls: ['./member-form.component.css']
+})
+
+export class MemberFormComponent implements OnInit {
+
+  memberForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    passwordForm: this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: PasswordValidator.match })
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  createMember() {
+    console.log(this.memberForm.value);
+  }
+  ngOnInit() {
+  }
+}
