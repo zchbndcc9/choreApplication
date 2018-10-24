ff416de465544126c9a9c48e3676e0ef14ace738
Zach Banducci <zbducci@gmail.com>
2018-10-24 12:08:11 -0500

    Seperated password form from member form for conditional rendering

--- frontend/src/app/member-form/member-form.component.ts
+++ frontend/src/app/member-form/member-form.component.ts
@@ -1,6 +1,6 @@
 import { PasswordValidator } from './../validators/password.validator';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 import { Member } from 'src/domain/models/member';
 
@@ -9,28 +9,47 @@ import { Member } from 'src/domain/models/member';
   templateUrl: './member-form.component.html',
   styleUrls: ['./member-form.component.css']
 })
-
 export class MemberFormComponent implements OnInit {
+  // Allows for a member and form type to be passed to the component
+  @Input() member: Member;
+  @Input() alreadyMember: boolean;
 
-  memberForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    username: ['', Validators.required],
-    isParent: [false],
-    passwordForm: this.fb.group({
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    }, { validator: PasswordValidator.match })
-  });
+  memberForm: FormGroup;
+  passwordForm: FormGroup;
 
+  // Input dependencies for the component
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.memberForm = this.fb.group({
+      firstName: [this.member.firstName || '', Validators.required],
+      lastName: [this.member.lastName || '', Validators.required],
+      username: [this.member.username || '', Validators.required],
+      isParent: [this.member.isParent || false]
+    });
+    if (!this.alreadyMember) {
+      this.passwordForm = this.fb.group(
+        {
+          password: ['', Validators.required],
+          confirmPassword: ['', Validators.required]
+        },
+        { validator: PasswordValidator.match }
+      );
+    }
+  }
+
   createMember() {
-    const member: Member = Object.assign({}, this.memberForm.value);
+    this.member = Object.assign({}, this.memberForm.value);
     // Some API call
     // Send data to members table
     this.memberForm.reset();
+    if (!this.alreadyMember) {
+      this.passwordForm.reset();
+    }
   }
-  ngOnInit() {
-  }
+
 }
