7ddaf74dc43bed7b2e32bab4bca0eda6f484c813
Zach Banducci <zbducci@gmail.com>
2018-10-24 21:20:39 -0500

    Added child page route to router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { FamilyMembersComponent } from './family-members/family-members.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 
 const routes: Routes = [
-  { path: 'family', component: ParentComponent }
+  { path: 'family', component: ParentComponent },
+  { path: 'family/members', component: FamilyMembersComponent }
 ];
 
 @NgModule({
