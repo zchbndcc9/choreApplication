0cb99d1075d03b574ec6c1876e98fced0427fdce
Zach Banducci <zbducci@gmail.com>
2018-11-07 22:41:23 -0600

    Added conditional styling for grounding on children card

--- frontend/src/app/members/components/member-card/member-card.component.ts
+++ frontend/src/app/members/components/member-card/member-card.component.ts
@@ -2,24 +2,27 @@ import { Member } from 'src/domain/models/member';
 import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-member-card',
+  selector: "app-member-card",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./member-card.component.css'],
+  styleUrls: ["./member-card.component.css"],
   template: `
     <div class="card">
-      <div class="card-header d-flex justify-content-between">
+      <div class="card-header d-flex justify-content-between align-items-center">
         <h3>{{ member.firstName + " " + member.lastName }}</h3>
+        <div *ngIf="member.grounded" class="badge badge-warning p-2">Grounded</div>
       </div>
       <div class="card-body">
       </div>
       <div class="card-footer d-flex justify-content-around">
         <button class="btn btn-primary" (click)="editMember()">Edit</button>
-        <button class="btn btn-warning" (click)="groundMember()">Ground</button>
+        <button [ngClass]="member.grounded ? 'btn btn-success' : 'btn btn-warning'"
+          (click)="groundMember()">
+          {{ member.grounded ? "Unground" : "Ground" }}
+        </button>
       </div>
-    </div>`,
+    </div>`
 })
 export class MemberCardComponent {
-
   @Input()
   member: Member;
 
