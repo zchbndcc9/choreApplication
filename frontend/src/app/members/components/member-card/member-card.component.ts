20f54fb2968970641fb08df96d5cb77f29fda469
Zach Banducci <zbducci@gmail.com>
2018-11-08 18:22:24 -0600

    Fixed naming issue with grounding

--- frontend/src/app/members/components/member-card/member-card.component.ts
+++ frontend/src/app/members/components/member-card/member-card.component.ts
@@ -1,3 +1,4 @@
+import { Child } from 'src/domain/models/child';
 import { Member } from 'src/domain/models/member';
 import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from '@angular/core';
 
@@ -9,28 +10,28 @@ import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from 
     <div class="card">
       <div class="card-header d-flex justify-content-between align-items-center">
         <h3>{{ member.firstName + " " + member.lastName }}</h3>
-        <div *ngIf="member.grounded" class="badge badge-warning p-2">Grounded</div>
+        <div *ngIf="member.isGrounded" class="badge badge-warning p-2">Grounded</div>
       </div>
       <div class="card-body">
       </div>
       <div class="card-footer d-flex justify-content-around">
         <button class="btn btn-primary" (click)="editMember()">Edit</button>
-        <button [ngClass]="member.grounded ? 'btn btn-success' : 'btn btn-warning'"
+        <button [ngClass]="member.isGrounded ? 'btn btn-success' : 'btn btn-warning'"
           (click)="toggleGround()">
-          {{ member.grounded ? "Unground" : "Ground" }}
+          {{ member.isGrounded ? "Unground" : "Ground" }}
         </button>
       </div>
     </div>`
 })
 export class MemberCardComponent {
   @Input()
-  member: Member;
+  member: Child;
 
   @Output()
-  edit = new EventEmitter<Member>();
+  edit = new EventEmitter<Child>();
 
   @Output()
-  ground = new EventEmitter<Member>();
+  ground = new EventEmitter<Child>();
 
   editMember() {
     this.edit.emit(this.member);
