b66999072330627520ba4457d19bf54ea11e484e
Zach Banducci <zbducci@gmail.com>
2018-11-13 16:12:36 -0600

    Added basic service for retrieving children by familyID

--- frontend/src/services/children/children.service.ts
+++ frontend/src/services/children/children.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Child } from 'src/domain/models/child';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { toArray, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChildrenService {
 
-  constructor() { }
+  protected baseUrl =
+    'http://ec2-18-222-217-233.us-east-2.compute.amazonaws.com:8080';
+
+  protected httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': ''
+    })
+  };
+
+  constructor(protected httpClient: HttpClient) {}
+
+  getChildren(familyId: number): Observable<Child[]> {
+    return this.httpClient.get<Child[]>(`${this.baseUrl}/family/${familyId}/children`, this.httpOptions)
+    .pipe(catchError(this.handleException));
+  }
+
+  protected handleException(exception: any) {
+    const message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
+    return Observable.throw(exception);
+  }
 }
